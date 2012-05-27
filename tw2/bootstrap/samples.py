73c1d12d3719e1679035bb898b36f091690d51e5
Ralph Bean <rbean@redhat.com>
2012-05-26 23:57:17 -0400

    Demo widget with validation errors.

--- tw2/bootstrap/samples.py
+++ tw2/bootstrap/samples.py
@@ -6,14 +6,33 @@ These samples will appear in the WidgetBrowser
 See http://toscawidgets.org/documentation/WidgetBrowser for more information
 """
 
+import tw2.core as twc
 from tw2.forms.samples import DemoChildren
 
-from widgets import *
+from tw2.bootstrap.widgets import *
 
 
-class DemoHorizontalForm(HorizontalForm, DemoChildren):
+class DemoHorizontalForm(HorizontalForm):
     legend = 'Hi, I\'m form!'
-    buttons = [SubmitButton(), ResetButton()]
+    title = TextField(validator=twc.Required)
+    # TODO -- uncomment this once we've got twb.SingleSelectField
+    #priority = SingleSelectField(options=['', 'Normal', 'High'])
+    description = TextArea
+    buttons = [SubmitButton, ResetButton]
+
+    def generate_output(self, displays_on):
+        """ We override this method *only* to forcefully invalidate the widget
+        for the demo.  We just want to show want the CSS looks like for a form
+        with an error in the input.
+        """
+
+        if hasattr(self, '_validated'):
+            return super(DemoHorizontalForm, self).generate_output(displays_on)
+
+        try:
+            DemoHorizontalForm.validate({})
+        except twc.ValidationError, e:
+            return e.widget.display()
 
 
 class DemoButton(Button):
