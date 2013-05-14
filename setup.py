b31a85ecf75360d81636893eb285707efb82d6f3
Moritz Schlarb <moschlar@metalabs.de>
2013-05-15 00:04:58 +0200

    Increase version so that my tests can run... :-]

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except:
 
 setup(
     name='tw2.bootstrap.forms',
-    version='2.0.0',
+    version='2.0.1',
     description="A drop-in replacement for tw2.forms but with bootstrap!",
     long_description=open('README.rst').read(),
     author='Moritz Schlarb',
